f5c72cb707d587173ab3826cc8c2d8e599ff8392
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-26 01:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var katzDeliLine = []
 
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ` + katzDeliLine.length + " in line."
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length === 0){
+  if (!katzDeliLine.length){
     return "There is nobody waiting to be served!"
   } else {
-    return "Currently serving " + katzDeliLine.shift() + ".";
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine(katzDeliLine){
   var line = []
   for(var i=0; i < katzDeliLine.length; i++){
-    line.push(i+1 + ". " + katzDeliLine[i])
+    line.push(`${i+1}. ${katzDeliLine[i]}`)
   }
-  if (line.length === 0){
+  if (!line.length){
     return "The line is currently empty."
   } else {
-    return "The line is currently: " + line.join(', ');
+    return `The line is currently: ${line.join(', ')}`;
   }
 }
